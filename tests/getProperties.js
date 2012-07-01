0834af501127d9659dab668d7a75de3276718c56
Fred Chien <fred@mandice.com>
2012-07-01 16:57:44 +0800

    add APIs to get information of power devices

--- tests/getProperties.js
+++ tests/getProperties.js
@@ -5,9 +5,9 @@ var power = new Power;
 power.init(function() {
 	console.log('CanSuspend: ' + power.canSuspend);
 	console.log('CanHibernate: ' + power.canHibernate);
+	console.log('Has Battery: ' + power.hasBattery);
 	console.log('OnBattery: ' + power.onBattery);
 	console.log('OnLowBattery: ' + power.onLowBattery);
-	console.log('OnAC: ' + power.isDocked);
 	console.log('Lid is Closed: ' + power.lidIsClosed);
 	console.log('Has Lid: ' + power.hasLid);
 });
